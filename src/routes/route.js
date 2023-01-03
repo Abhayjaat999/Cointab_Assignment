fa9a343a95375f008e8778106bca42c336620983
Abhay999 <abhayjaat999@gmail.com>
2023-01-03 22:00:17 +0530

    Final

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const path = require('path')
 const body_Parser = require('body-parser')
 const userController= require("../controllers/controllers")
 const router = express.Router();
+
 router.use(body_Parser.urlencoded({extended:true}));
 
 router.get('/',userController.isLoggedIn, (req, res) => {
@@ -12,10 +13,12 @@ router.get('/',userController.isLoggedIn, (req, res) => {
 router.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname ,"../public/js","register.html"))
 });
+router.post('/register', userController.register)
 
 router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname ,"../public/js","login.html"))
 });
+router.post('/login', userController.login);
 
 router.get('/profile', userController.isLoggedIn, (req, res) => {
     if (req.user) {
@@ -24,15 +27,12 @@ router.get('/profile', userController.isLoggedIn, (req, res) => {
         res.sendFile(path.join(__dirname ,"../public/js","login.html"))
     }
 })
+router.get('/logout', userController.logout);
 
 router.all("/*", (req, res) => 
 { console.log(req.params.productId)
     res.status(400).send({ status: false, message: "Endpoint is not correct" }) })
 
-    ////////////
 
-    router.post('/register', userController.register)
-    router.post('/login', userController.login);
-    router.get('/logout', userController.logout);
 
 module.exports = router;
